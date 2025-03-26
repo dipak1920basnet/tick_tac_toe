e9f5321f9a5a295015a35a7cf691e99b85b88f3d
dipak1920basnet <dipak1920basnet@gmail.com>
2025-03-26 11:56:01 +0545

    add player and their marker

--- script.js
+++ script.js
@@ -1,15 +1,39 @@
 document.addEventListener('DOMContentLoaded', function(){
-    function move(a)
+    function move()
     {
+        let turn = 0;
         let m = document.querySelectorAll(".play_ground button")
     m.forEach(element => {
         element.addEventListener('click', function() {
+            if (turn == 0)
+            {
+                element.innerText = player_one.make_move()
+                turn = 1
+            }
+            else{
+                element.innerText = player_two.make_move()
+                turn = 0
+            }
             element.innerText = a;
         })
     });
     }
 
-    move("X")
+    move()
+
+    function player(name, marker)
+    {
+        this.name = name;
+        this.marker = marker;
+    }
+    player.prototype.make_move = function()
+    {
+        return (this.marker)
+    }
+
+    player_one = new player("Dipak","X")
+    player_two = new player("Reawanta","O")
+    
 
     let control = document.querySelectorAll(".controls button")
     control.forEach(element => {
